ad1c940112f4e657cfd7431e4b98934238dce159
Katie B <katiebittner1@gmail.com>
2020-09-08 19:06:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -18,6 +18,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   
   reversed_sentence = array.collect do |word|
+    word = word.split("")
     reversed_word = word.collect do |letter|
       reversed_word.prepend(letter)
     end
