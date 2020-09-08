468c2f7e5890491c0917c332e29db0362cc413cb
Katie B <katiebittner1@gmail.com>
2020-09-08 18:59:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,15 @@
-def reverse_each_word(string)
-  array = string.split(" ")
-  reversed_sentence = ""
+#def reverse_each_word(string)
+#  array = string.split(" ")
+#  reversed_sentence = ""
+#  
+#  array.each do |word|
+#    reversed_word = ""
+#    word = word.split("")
+#    word.each do |letter|
+#      reversed_word.prepend(letter)
+#    end
+#    reversed_sentence.concat(reversed_word + " ")
+#  end
   
-  array.each do |word|
-    reversed_word = ""
-    word = word.split("")
-    word.each do |letter|
-      reversed_word.prepend(letter)
-    end
-    reversed_sentence.concat(reversed_word + " ")
-  end
-  
-  reversed_sentence.chop
-end
+#  reversed_sentence.chop
+#end
